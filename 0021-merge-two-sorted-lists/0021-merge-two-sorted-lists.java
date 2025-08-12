7e9f2ee84cbeb3cf5c9f4e799ad209822537daf8
harinietturouthu <23951A1239@iare.ac.in>
2025-08-12 12:10:15 +0530

    Time: 0 ms (100%), Space: 42.4 MB (75.76%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,8 +10,8 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy=new ListNode();
-        ListNode current=dummy;
+        ListNode d=new ListNode();
+        ListNode current=d;
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
                 current.next=list1;
@@ -36,7 +36,7 @@ class Solution {
         if(list2!=null){
             current.next=list2;
         }
-        return dummy.next;
+        return d.next;
  
     }
 }

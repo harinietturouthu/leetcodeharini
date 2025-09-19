597562179e0a7f991e1e25a69a4929e41974ed0e
harinietturouthu <23951A1239@iare.ac.in>
2025-09-19 14:50:22 +0530

    Time: 492 ms (97.04%), Space: 0B (100%) - LeetHub

---
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+select round(avg(b.event_date is not null),2)fraction from (select Player_id,min(event_date) event_date from activity group by player_id ) a left join activity b on a.player_id=b.player_id and datediff(a.event_date,b.event_date)=-1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

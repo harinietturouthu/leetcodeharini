2d46d6faa13f9bdbcc88893e15e9af4711ab7b2d
harinietturouthu <23951A1239@iare.ac.in>
2026-03-29 16:53:37 +0530

    Time: 5 ms (86.48%), Space: 44.5 MB (55.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -6,19 +6,18 @@ class Solution {
         int i=n-1;
         int j=n-1;
         while(i>=0){
-            if(s.charAt(i)==' '&& i< j){
+            if(s.charAt(i)==' '&i<j){
                 result.append(s.substring(i+1,j+1));
                 result.append(" ");
                 j=i-1;
             }
+            //this lines are ther because the j should the reach the word it should not be in space*/ 
             while(j >= 0 && s.charAt(j) == ' '){
                     j--;
                 }
-
             i--;
         }
         result.append(s.substring(0,j+1));
-        return result.toString().trim();
-        
+        return result.toString().trim(); 
 }
 }

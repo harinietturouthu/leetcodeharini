cd732c30a93d121e1e9e4b92a8a26fa654ae288d
harinietturouthu <23951A1239@iare.ac.in>
2026-05-13 16:12:45 +0530

    Time: 1 ms (99.96%), Space: 46.8 MB (81.25%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,18 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-      int n=nums.length;
-      if(nums.length==1) return nums[0];
-      for(int i=0;i<n;i++){
-        boolean found=false;
-        for(int j=0;j<n;j++){
-            if(i!=j&&nums[i]==nums[j]){
-                found=true;
-                break;
-            }
-        }
-        if(found==false)
-        return nums[i];
-             }  
-             return -1;
+      int rem=0;
+      for(int i=0;i<nums.length;i++){
+        rem=rem^nums[i];
+        
+      }
+      return rem;
     }
 }

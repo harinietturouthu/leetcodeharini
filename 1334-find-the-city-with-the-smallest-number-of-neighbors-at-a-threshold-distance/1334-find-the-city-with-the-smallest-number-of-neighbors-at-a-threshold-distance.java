559f0ae3fcf6a102ecff67ae7d343eb12f800522
harinietturouthu <23951A1239@iare.ac.in>
2026-07-02 19:24:33 +0530

    Time: 13 ms (33.34%), Space: 46.5 MB (59.56%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,74 +1,58 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int INF = 1000000000;
-
-        // Step 1: create distance matrix
-        int[][] dist = new int[n][n];
-
-        // Step 2: initialize matrix
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-
-                if(i == j){
-                    dist[i][j] = 0;
-                }
-                else{
-                    dist[i][j] = INF;
-                }
-            }
-        }
-
-        // Step 3: fill edges
-        for(int i = 0; i < edges.length; i++){
-
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-
-            dist[u][v] = wt;
-            dist[v][u] = wt;
-        }
-
-        // Step 4: Floyd Warshall
-        for(int k = 0; k < n; k++){
-
-            for(int i = 0; i < n; i++){
-
-                for(int j = 0; j < n; j++){
-
-                    if(dist[i][k] != INF && dist[k][j] != INF){
-
-                        dist[i][j] = Math.min(
-                                dist[i][j],
-                                dist[i][k] + dist[k][j]
-                        );
+       List<List<int[]>> adj=new ArrayList<>();
+       for(int i=0;i<n;i++){
+        adj.add(new ArrayList<>());
+       }
+       for(int i=0;i<edges.length;i++){
+        int from=edges[i][0];
+        int to=edges[i][1];
+        int weight=edges[i][2];
+        adj.get(from).add(new int[]{to,weight});
+        adj.get(to).add(new int[]{from,weight});
+       }
+       int[][] dist=new int[n][n];
+       for(int i=0;i<n;i++){
+       Arrays.fill(dist[i],Integer.MAX_VALUE);}
+       for(int i=0;i<n;i++){
+        dist[i][i]=0;
+       }
+    for(int i=0;i<adj.size();i++){
+        for(int[] ne:adj.get(i)){
+            int h=ne[0];
+            int t=ne[1];
+            dist[i][h]=t;
                     }
-                }
+    }
+       for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){ 
+                    if(dist[i][k]!=Integer.MAX_VALUE&&dist[j][k]!=Integer.MAX_VALUE){
+dist[i][j]=Math.min(dist[i][j],dist[i][k]+dist[k][j]);}
+    }
+}
+       }
+       int [] a=new int[n];
+       for(int i=0;i<dist.length;i++){
+        for(int j=0;j<dist[0].length;j++){
+            if(i!=j&&dist[i][j]<=distanceThreshold){
+                a[i]++;
             }
+
         }
-
-        // Step 5: find city with smallest reachable cities
-        int minCount = Integer.MAX_VALUE;
-        int ans = -1;
-
-        for(int i = 0; i < n; i++){
-
-            int count = 0;
-
-            for(int j = 0; j < n; j++){
-
-                if(i != j && dist[i][j] <= distanceThreshold){
-                    count++;
-                }
-            }
-
-            // tie → choose greater city number
-            if(count <= minCount){
-                minCount = count;
-                ans = i;
-            }
+       }
+       List<Integer> o=new ArrayList<>();
+       int min=Integer.MAX_VALUE;
+       for(int i=0;i<a.length;i++){
+          min=Math.min(min,a[i]);
+       }      
+       for(int i=0;i<a.length;i++){
+        if(a[i]==min){
+            o.add(i);
         }
-
-        return ans; 
+       }
+      
+        return o.get(o.size()-1);
+      
     }
 }
